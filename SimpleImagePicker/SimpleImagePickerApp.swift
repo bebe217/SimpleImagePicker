95a1ef450fe09b47d875b8d8747c900ed8125e9b
bebeda <bebe217@naver.com>
2024-09-04 17:18:39 +0900

    Initial Commit

---
+++ SimpleImagePicker/SimpleImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleImagePickerApp.swift
+//  SimpleImagePicker
+//
+//  Created by bebe on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
